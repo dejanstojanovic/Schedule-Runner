57e9a8b2ea79aee207e5ba74e307baf1285d0dfb
Dejan Stojanovic <d.stojanovic@hotmail.com>
2015-11-16 09:28:27 +0400

    Improved logging to Windows log

--- src/ScheduleRunner/ExceptionLog.cs
+++ src/ScheduleRunner/ExceptionLog.cs
@@ -10,15 +10,29 @@ namespace Scheduler
 {
     public class ExceptionLog : ILog
     {
-        //https://support.microsoft.com/en-us/kb/307024
-
-
+        #region Fields
         private string source = Assembly.GetEntryAssembly().GetName().Name;
-        private string log = "Application";
-        private string evnt = "Exception";
+        public const string DEFAULT_LOG = "Application";
+        private string log = DEFAULT_LOG;
 
+        #endregion
 
+        #region Constructors
         public ExceptionLog()
+        {
+            this.CreateSource();
+        }
+
+
+        public ExceptionLog(string source, string log = DEFAULT_LOG)
+        {
+            this.source = source;
+            this.log = log;
+            this.CreateSource();
+        }
+        #endregion
+
+        private void CreateSource()
         {
             if (!EventLog.SourceExists(source))
             {
@@ -26,11 +40,17 @@ namespace Scheduler
             }
         }
 
+        /// <summary>
+        /// Logs exception to Windows event log
+        /// </summary>
+        /// <param name="ex">Exception to be logged</param>
         public void Log(Exception ex)
         {
             Task.Run(() =>
             {
-                EventLog.WriteEntry(this.source, this.evnt, EventLogEntryType.Error);
+                EventLog.WriteEntry(this.source,
+                                    ex.StackTrace,
+                                    EventLogEntryType.Error);
             });
         }
     }
